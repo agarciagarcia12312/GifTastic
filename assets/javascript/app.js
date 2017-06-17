626bb01f8b360a2ec91107fce90a009dac5a9958
andy garcia <andy garcia>
2017-06-16 23:08:19 -0600

    fully working just need more css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ var count = 0;
 // Submit button add the val in the inout to the array 
 // and deletes what ever the user was typing
 $(document).ready(function() {
-	console.log("yes");
+	
 	
 	function createButton (x) {
 		var searchButton = $("<button>")
@@ -30,6 +30,9 @@ $(document).ready(function() {
 	});
 	
 	$(document.body).on("click",".search",function() {
+		
+		$("#Gifs").html("");
+
 		var searchId = $(this).attr("value");
 		var key = "&api_key=dc6zaTOxFJmzC";
 		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchId + key;
@@ -40,13 +43,30 @@ $(document).ready(function() {
 		}).done(function(responce) {
 			console.log(responce.data[i].rating);
 			for (i=0; i < 10; i++) {
-				var newdiv = $("<div>")
-				var rating = ("<h3>Rating:" + responce.data[i].rating + "</h3>" + "<br>" +'<img src=' + responce.data[i].images.fixed_width_still.url + '/>' )
-				newdiv.append(rating);
-				// newdiv.append('<img src=' + responce.data[i].images.fixed_width_still.url + '/>')
-				// newdiv.append('<img src=' + responce.data[i].images.fixed_width_still.url  '/>');
-				$("#Gifs").append(newdiv); 
+				var newdiv = $("<div>");
+				var stillURL =  responce.data[i].images.fixed_width_still.url ;
+				var gifData = ("<h3>Rating:" + responce.data[i].rating + "</h3>" +'<img class="giphys" id="' +i + '"src=' + stillURL + '/>' )
+				newdiv.attr("value", i);
+				newdiv.append(gifData);
+				$("#Gifs").append(newdiv);
+
+				
 			}
+
+			$(document.body).on("click",".giphys", function() {
+
+					var y = $(this).attr("id");
+					var still = $(this).attr("src");
+					var check = responce.data[y].images.fixed_width_still.url+ "/";
+					console.log(still);
+					console.log(check);
+					if (check == still) {
+						$(this).attr("src", responce.data[y].images.fixed_width.url)
+					}
+					else {
+						$(this).attr("src", responce.data[y].images.fixed_width_still.url +"/")
+					}	
+				})
 
 		})
 
